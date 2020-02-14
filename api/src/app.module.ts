c3ea9734c5001f07ddcca048aa7c36ff5c46d71a
Rowad <rowadwo@gmail.com>
2020-02-14 02:49:56 +0200

    installed mongo driver and connected successfully...

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
